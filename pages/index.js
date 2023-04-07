18ae486d12d9fef9afb08d904835fdf7744cfbf3
Lais Furtado <lda-silva-lopes-furt@myseneca.ca>
2023-04-06 21:06:45 -0400

    adding alt

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
   return (
     <>
      
-        <Image src="https://upload.wikimedia.org/wikipedia/commons/3/30/Metropolitan_Museum_of_Art_%28The_Met%29_-_Central_Park%2C_NYC.jpg" fluid rounded/>
+        <Image src="https://upload.wikimedia.org/wikipedia/commons/3/30/Metropolitan_Museum_of_Art_%28The_Met%29_-_Central_Park%2C_NYC.jpg" alt="Front view of Metropolitan Museum of Art" fluid rounded/>
         <Row>
         <Col md={6}>
           <br/>
